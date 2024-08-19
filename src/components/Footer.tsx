78192c62f3975bdf0d729b2d24ddc278ef032c4b
jeremnds <jrmymnds@gmail.com>
2024-08-19 18:49:23 +0200

    fix: resolve footer margins

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Links from "./Links";
 
 export default function Footer() {
   return (
-    <footer className=" w-full text-sm h-20 ">
+    <footer className=" w-full text-sm h-20 flex items-center ">
       <Links />
     </footer>
   );
